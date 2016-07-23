4d295ccdf94ade9efbd396c3ed3536c71676ace2
Your Name <lsx@163.com>
2016-07-23 08:26:54 +0800

    toast已用

---
+++ app/src/main/java/com/blue/activitylifecycle/ActivitySecond.java
@@ -0,0 +1,11 @@
+package com.blue.activitylifecycle;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class ActivitySecond extends AppCompatActivity {
+
+}
