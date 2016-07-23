da54c3b3955868e421c994b172e0a8eb5db680b7
Your Name <lsx@163.com>
2016-07-23 10:22:47 +0800

    MainActivity传递消息到ActivitySencod的TextView

--- app/src/main/java/com/blue/activitylifecycle/ActivitySecond.java
+++ app/src/main/java/com/blue/activitylifecycle/ActivitySecond.java
@@ -4,16 +4,21 @@ import android.app.Application;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
 public class ActivitySecond extends AppCompatActivity {
+    private TextView textView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String inforContent =getIntent().getStringExtra("name");
-        Toast.makeText(ActivitySecond.this,inforContent,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(ActivitySecond.this,inforContent,Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_second);
+        textView=(TextView)findViewById(R.id.second_activity_text_view);
+        textView.setText(inforContent);
     }
 }
